848eea521b40533bf376d6cbc11fbf63db4cd558
nirab123456 <rifatulislam210@gmail.com>
2026-05-24 20:46:32 -0400

    Subdivision2x16Plus2x8InternalMode48CellModel->ADDED::IN->Mode48CellModels.hpp

--- core/headers/AdaptivePackedCellContainer/PackedCell/Mode48CellModels.hpp
+++ core/headers/AdaptivePackedCellContainer/PackedCell/Mode48CellModels.hpp
@@ -72,5 +72,90 @@ namespace PredictedAdaptedEncoding
     };
 
 
+    struct Subdivision2x16Plus2x8InternalMode48CellModel
+    {
+        static constexpr unsigned FIRST_16BIT_SHIFT_0 = 0u;
+        static constexpr unsigned SECOND_16BIT_SHIFT_1 = 16u;
+        static constexpr unsigned FIRST_8BIT_SHIFT_2 = 24u;
+        static constexpr unsigned SECOND_8BIT_SHIFT_3 = 32u;
+
+        static constexpr uint64_t MASK_LOW_8 = MaskLowNBits(8);
+
+        static packed64_t Compose4Unsigned16x2Plus8x2IndependendentInMode48(
+            uint16_t lowest_16bit_0,
+            uint16_t low_16bit_1,
+            uint8_t high_8bit_2,
+            uint8_t highest_8bit_3,
+            meta16_t meta16
+        ) noexcept
+        {
+            const uint64_t raw48 = (
+                Pack2x16Plus2x8UnsignedSubdivision_(
+                    lowest_16bit_0, low_16bit_1,
+                    high_8bit_2, highest_8bit_3
+                ) & MaskLowNBits(CLK_B48)
+            );
+            return PackedCell64_t::ComposeCLK48u_64(raw48, meta16);
+        }
+
+        static uint64_t Pack2x16Plus2x8UnsignedSubdivision_(
+            uint16_t low16_0,
+            uint16_t low16_1,
+            uint8_t high8_2,
+            uint8_t high8_3
+        ) noexcept
+        {
+            return (uint64_t{low16_0} << FIRST_16BIT_SHIFT_0) |
+                (uint64_t{low16_1} << SECOND_16BIT_SHIFT_1) |
+                (uint64_t{high8_2} << FIRST_8BIT_SHIFT_2) |
+                (uint64_t{high8_3} << SECOND_8BIT_SHIFT_3);
+        }
+
+        static uint16_t ExtractLowestFirstLow16Bit0_(uint64_t raw48) noexcept
+        {
+            return static_cast<uint16_t>((raw48 >> FIRST_16BIT_SHIFT_0) & Subdevision16x3InternalMode48CellModel::MASK_LOW_16);
+        }
+
+        static uint16_t ExtractSecondLow16Bit1_(uint64_t raw48) noexcept
+        {
+            return static_cast<uint16_t>((raw48 >> SECOND_16BIT_SHIFT_1) & Subdevision16x3InternalMode48CellModel::MASK_LOW_16);
+        }
+
+        static uint8_t ExtractHigh8Bit2_(uint64_t raw48) noexcept
+        {
+            return static_cast<uint8_t>((raw48 >> FIRST_8BIT_SHIFT_2) & MASK_LOW_8);
+        }
+
+        static uint8_t ExtractHighestHigh8Bit3_(uint64_t raw48) noexcept
+        {
+            return static_cast<uint8_t>((raw48 >> SECOND_8BIT_SHIFT_3) & MASK_LOW_8);
+        }
+
+        static bool IsThisCellAFourSubdevision_48t(packed64_t packed_cell) noexcept
+        {
+            return PackedCell64_t::ExtractModeOfPackedCellFromPacked(packed_cell) == PackedMode::CLOCK_OR_VALUE_48 &&
+                PackedCell64_t::ExtractRelOffset48FromPacked(packed_cell) == RelOffsetMode48::FOUR_SUBDIVISION_2x16_AND_2x8 &&
+                PackedCell64_t::ExtractLocalityFromPacked(packed_cell) != PackedCellLocalityTypes::ST_EXCEPTION_BIT_FAULTY;
+        }
+
+        static bool ExtractAll4SubdivisionInOneGoIfValid_(
+            uint64_t raw48, uint16_t& lowest_16bit_0, uint16_t& low_16bit_1,
+            uint8_t& high_8bit_2, uint8_t& highest_8bit_3
+        ) noexcept
+        {
+            if (raw48 == PackedCell64_t::PACKED_CELL_SENTINAL)
+            {
+                return false;
+            }
+            lowest_16bit_0 = ExtractLowestFirstLow16Bit0_(raw48);
+            low_16bit_1 = ExtractSecondLow16Bit1_(raw48);
+            high_8bit_2 = ExtractHigh8Bit2_(raw48);
+            highest_8bit_3 = ExtractHighestHigh8Bit3_(raw48);
+            return true;
+        }
+
+    };
+
+
 
 }
